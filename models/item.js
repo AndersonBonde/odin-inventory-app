a7ba36885b88419cd6e42a2149712b2338746da1
Anderson <andersonbonde@gmail.com>
2024-04-16 11:04:56 -0300

    Create schemas & models

---
+++ models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = ({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+  price: { type: Number, required: true },
+  numberInStock: { type: Number, required: true },
+});
+
+ItemSchema.virtual('url').get(function() {
+  return `/inventory/item/${this._id}`;
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
